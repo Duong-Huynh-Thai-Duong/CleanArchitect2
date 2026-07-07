6ddf3cc917c072353f4a81fa0f0cc35f96fe1ae6
Duong-Huynh-Thai-Duong <duonghuynhthaiduong18@gmail.com>
2026-07-07 21:26:18 +0700

    Fix comment typo in Program.cs

--- CleanArchitect.API/Program.cs
+++ CleanArchitect.API/Program.cs
@@ -7,7 +7,7 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container yoyoyo.
+// Add services to the container.
 builder.Services.AddDbContext<EBikeShopDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("EBikeShopConnection")
